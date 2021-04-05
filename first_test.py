cc5995adad7e494f4b030283ff0b61d65170a1ef
Viacheslav Gurov <gurov>
2021-04-04 23:09:40 -0400

    dt 04 april 2021

--- first_test.py
+++ first_test.py
@@ -5,3 +5,4 @@ def func(x):
 
 def test_method():
     assert func(3) == 8
+
--- multiple_tests.py
+++ multiple_tests.py
@@ -7,7 +7,7 @@ def test_method1():
     assert x == y
 
 @pytest.mark.two
-def test_method1():
+def test_method2():
     a = 15
     b = 20
     assert a + 5 == b
