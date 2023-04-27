c90e0c76348c806f02b8e52063112b1a420398b8
Vic Gurov <gurovvic@gmail.com>
2023-04-27 15:33:19 -0500

    dt 27 apr 2023

---
+++ locators_amazon.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+# locators
+SEARCH_FIELD = (By.ID, 'twotabsearchtextbox')
+TEXT_FIELD = (By.XPATH, "//span[@class='a-color-state a-text-bold']")
--- test_automation_search.py
+++ test_automation_search.py
@@ -1,26 +1,28 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from locators_amazon import *
+
 
 class TestAmazon:
-    search_words = ('dress', 'shoes', 'toys', 'dress')
-
+    search_words = ('dress', 'shoes', 'toys', 'shirts')
     driver = ''
 
     def setup_method(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 15)
         self.driver.get('https://www.amazon.com/')
 
     @pytest.mark.parametrize('search_query', search_words)
-    def test_amazon_search_dress(self, search_query):
-        search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
+    def test_amazon_search_whatever(self, search_query):
+        search = self.driver.find_element( *SEARCH_FIELD)
         search.send_keys(search_query, Keys.ENTER)
 
         expected_text = f'\"{search_query}\"'
-        actual_text = self.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
+        actual_text = self.driver.find_element( *TEXT_FIELD).text
 
         assert expected_text == actual_text, f'Error.Expected text: {expected_text}, but actual text: {actual_text}'
 
